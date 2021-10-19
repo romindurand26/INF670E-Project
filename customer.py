da66db74a2831222fc6e601d9cadfeca4899fa2f
Romin <romin.durand@citronmauve.com>
2021-10-19 12:59:24 +0200

    customer table

--- customer.py
+++ customer.py
@@ -1,52 +1,38 @@
 from table import Table_column, Table_row, Row
 
 
-class Customers_column(Table_column):
+class Customer_column(Table_column):
     def __init__(self):
-        self.primary_key_name = ['id']
-        self.foreign_key_name = ['name']
-        self.id = []
+        self.primary_key_name = ['custkey']
+        self.foreign_key_name = ['nationkey']
+        self.custkey = []
         self.name = []
-        self.age = []
+        self.address = []
+        self.nationkey = []
+        self.phone = []
+        self.acctbal = []
+        self.mktsegment = []
+        self.comment = []
 
-        super().__init__('customer')
+        super().__init__('CUSTOMER')
 
 
 class Customer(Row):
     def __init__(self, values):
-        self.id = None
+        self.custkey = None
         self.name = None
-        self.age = None
+        self.address = None
+        self.nationkey = None
+        self.phone = None
+        self.acctbal = None
+        self.mktsegment = None
+        self.comment = None
 
         super().__init__(values)
 
 
-class Customers_row(Table_row):
+class Customer_row(Table_row):
     def __init__(self):
-        self.primary_key_name = ['id']
-        self.foreign_key_name = ['name']
-        super().__init__(Customer, 'customer')
-
-
-storage = 'column'
-show = 2
-
-if storage == 'row':
-    customers = Customers_row()
-else:
-    customers = Customers_column()
-
-if show == 1:
-    customers.add(["002", "Yasmine", "24"])
-    customers.add(["007", "Romin", "77"])
-    customers.add(["000", "Oumaima", "5"])
-    customers.add(["001", "Louay", "18"])
-
-if show == 2:
-    customers.add(["111", "Hubert", "55"])
-    customers.add(["222", "Jean", "1"])
-
-if storage == 'row':
-    customers.dump_row()
-if storage == 'column':
-    customers.dump_column()
+        self.primary_key_name = ['custkey']
+        self.foreign_key_name = ['nationkey']
+        super().__init__(Customer, 'CUSTOMER')
---
+++ customer_exemple.py
@@ -0,0 +1,52 @@
+from table import Table_column, Table_row, Row
+
+
+class Customers_column(Table_column):
+    def __init__(self):
+        self.primary_key_name = ['id']
+        self.foreign_key_name = ['name']
+        self.id = []
+        self.name = []
+        self.age = []
+
+        super().__init__('customer')
+
+
+class Customer(Row):
+    def __init__(self, values):
+        self.id = None
+        self.name = None
+        self.age = None
+
+        super().__init__(values)
+
+
+class Customers_row(Table_row):
+    def __init__(self):
+        self.primary_key_name = ['id']
+        self.foreign_key_name = ['name']
+        super().__init__(Customer, 'customer')
+
+
+storage = 'column'
+show = 2
+
+if storage == 'row':
+    customers = Customers_row()
+else:
+    customers = Customers_column()
+
+if show == 1:
+    customers.add(["002", "Yasmine", "24"])
+    customers.add(["007", "Romin", "77"])
+    customers.add(["000", "Oumaima", "5"])
+    customers.add(["001", "Louay", "18"])
+
+if show == 2:
+    customers.add(["111", "Hubert", "55"])
+    customers.add(["222", "Jean", "1"])
+
+if storage == 'row':
+    customers.dump_row()
+if storage == 'column':
+    customers.dump_column()
